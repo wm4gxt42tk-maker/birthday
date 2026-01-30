3829f1b99ed31d5de7379b0187b8a913ca96cc31
Sprite007 <wm4gxt42tk@privaterelay.appleid.com>
2026-01-30 23:59:25 +0545

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ import runtimeErrorOverlay from "@replit/vite-plugin-runtime-error-modal";
 import { metaImagesPlugin } from "./vite-plugin-meta-images";
 
 export default defineConfig({
-  base: '/biditbirthday/',
+  base: '/birthday/',
   plugins: [
     react(),
     runtimeErrorOverlay(),
